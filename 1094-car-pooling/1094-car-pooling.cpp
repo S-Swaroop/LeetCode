95a4308517b320f8dbac3f9da374ea6c61a5d0d0
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-06 10:53:00 +0530

    Time: 14 ms (45.94%), Space: 10.2 MB (60.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a , vector<int>& b){
-        if(a[1] == b[1])    return a[2] < b[2] ; 
-        else    return a[1] < b[1] ; 
-    }
+    // static bool comp(vector<int>& a , vector<int>& b){
+    //     if(a[1] == b[1])    return a[2] < b[2] ; 
+    //     else    return a[1] < b[1] ; 
+    // }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin() , trips.end() , comp) ; 
+        // sort(trips.begin() , trips.end() , comp) ; 
         int n = trips.size() ; 
         vector<int> capacity_at_dist(1010 , 0) ; 
         for(int i = 0 ; i < n ; i++){
