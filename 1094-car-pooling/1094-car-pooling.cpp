eff34cb78e3de17e5e01d062df4080e7cdeefe83
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-06 10:59:44 +0530

    Time: 19 ms (31.82%), Space: 9.8 MB (91.12%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         int n = trips.size() ; 
-        vector<int> capacity_at_dist(1002 , 0) ; 
-        for(int i = 0 ; i < n ; i++){
-            capacity_at_dist[trips[i][1] + 1] += trips[i][0] ;
-            capacity_at_dist[trips[i][2] + 1] -= trips[i][0] ; 
+        int _max = 0 , _min = 1001 ;
+        for(auto &i : trips){
+            _max = max(_max , i[2]) ;
+            _min = min(_min , i[1]) ; 
         }
-        for(int i = 1 ; i <= 1000 ; i++){
+        vector<int> capacity_at_dist(_max - _min + 10 , 0) ; 
+        for(int i = 0 ; i < n ; i++){
+            capacity_at_dist[trips[i][1] - _min + 1] += trips[i][0] ;
+            capacity_at_dist[trips[i][2] - _min + 1] -= trips[i][0] ; 
+        }
+        for(int i = 1 ; i <= _max - _min + 1 ; i++){
              capacity_at_dist[i] += capacity_at_dist[i - 1] ;
             if(capacity_at_dist[i] > capacity)  return false ; 
         }
