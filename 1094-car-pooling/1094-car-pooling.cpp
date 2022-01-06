fa41b6ebb7a0961eb54d5f6359e6a49fcea6f425
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-06 11:09:45 +0530

    Time: 17 ms (34.59%), Space: 12.7 MB (6.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
+    /*
+    -----
+       -----
+        --
+             ---------
+    we need to check if at any distance the number of passengers required to be picked up 
+    is greater than capacity or not
+    a simple partial sum trick will do 
+    lets say you need to pick 'x' passengers at point 'l' and drop at point 'r',
+    just add +x at point[l] and add -x at point[r]
+    so while going left to right we will consider +x passengers at and after point[l] and
+    -x passengers after and at point[r]
+    */
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int n = trips.size() ; 
-        int _max = 0 , _min = 1001 ;
-        for(auto &i : trips){
-            _max = max(_max , i[2]) ;
-            _min = min(_min , i[1]) ; 
+        vector<int> capacity_at_dist(10005 , 0) ;
+        int n = trips.size() ;
+        for (int i = 0 ; i < n ; i++){
+            capacity_at_dist[trips[i][1]] += trips[i][0] ;
+            capacity_at_dist[trips[i][2]] -= trips[i][0] ;
         }
-        vector<int> capacity_at_dist(_max - _min + 10 , 0) ; 
-        for(int i = 0 ; i < n ; i++){
-            capacity_at_dist[trips[i][1] - _min + 1] += trips[i][0] ;
-            capacity_at_dist[trips[i][2] - _min + 1] -= trips[i][0] ; 
+        int capacity_at_any_dist = 0 ;
+        for (int i = 0 ; i<= 10000 ; i++){
+            capacity_at_any_dist += capacity_at_dist[i] ;
+            if (capacity_at_any_dist > capacity)    return false;
         }
-        for(int i = 1 ; i <= _max - _min + 1 ; i++){
-             capacity_at_dist[i] += capacity_at_dist[i - 1] ;
-            if(capacity_at_dist[i] > capacity)  return false ; 
-        }
-        return true ; 
+        return true; 
     }
 };
