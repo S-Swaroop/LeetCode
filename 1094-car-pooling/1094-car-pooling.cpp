4a194167cea9ebcce08dc3eb27bf712722e68e16
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-06 10:51:43 +0530

    Time: 40 ms (10.46%), Space: 10.4 MB (39.70%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static bool comp(vector<int>& a , vector<int>& b){
+        if(a[1] == b[1])    return a[2] < b[2] ; 
+        else    return a[1] < b[1] ; 
+    }
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        sort(trips.begin() , trips.end() , comp) ; 
+        int n = trips.size() ; 
+        vector<int> capacity_at_dist(1010 , 0) ; 
+        for(int i = 0 ; i < n ; i++){
+            capacity_at_dist[trips[i][1] + 1] += trips[i][0] ;
+            capacity_at_dist[trips[i][2] + 1] -= trips[i][0] ; 
+        }
+        vector<int> pref(1010 , 0) ;
+        for(int i = 1 ; i <= 1000 ; i++){
+             pref[i] = capacity_at_dist[i] + pref[i - 1] ;
+            if(pref[i] > capacity)  return false ; 
+            cout << pref[i] << " " ;
+        }
+        cout << endl ; 
+        return true ; 
+    }
+};
