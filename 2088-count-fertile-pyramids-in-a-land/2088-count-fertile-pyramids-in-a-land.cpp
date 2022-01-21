822cf240d509b403eaf30e760f52f0da1f633f2b
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-22 00:07:21 +0530

    Time: 290 ms (31.10%), Space: 32.2 MB (93.04%) - LeetHub

--- 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.cpp
+++ 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.cpp
@@ -7,16 +7,22 @@ public:
         return ans ;
     }
 private: 
-    int count(vector<vector<int>> grid){
+    int count(vector<vector<int>> &grid){
         int n = grid.size() , m = grid[0].size() ; 
+        vector<vector<int>> f(2 , vector<int>(m , 0)) ;         
         int ans = 0 ; 
+        for(int i = 0 ; i < m ; i++)    f[0][i] = grid[0][i] ; 
         for(int i = 1 ; i < n ; i++){
+            f[1][0] = grid[i][0] ; 
+            f[1][m - 1] = grid[i][m - 1] ;
             for(int j = 1 ; j < m - 1 ; j++){
-                 if(grid[i][j] && grid[i - 1][j]){
-                     grid[i][j] = min({grid[i - 1][j - 1] , grid[i - 1][j + 1]}) + 1 ; 
-                     ans += grid[i][j] - 1 ; 
+                f[1][j] = grid[i][j] ;
+                 if(f[1][j] && f[0][j]){
+                     f[1][j] = min({f[0][j - 1] , f[0][j + 1]}) + 1 ; 
+                     ans += f[1][j] - 1 ; 
                  }
             }
+            swap(f[1] , f[0]) ; 
         }
         return ans ; 
     }
