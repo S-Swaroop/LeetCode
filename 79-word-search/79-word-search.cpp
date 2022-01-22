bc07f0e21fd48ce02f85d09e4fdc85c43480d489
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-23 01:56:15 +0530

    Time: 568 ms (35.29%), Space: 7.9 MB (60.44%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size() , m = board[0].size() ; 
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(dfs(i , j , 0 , board , word)){
+                    return true ; 
+                }
+            }
+        }
+        return false ; 
+    }
+private: 
+    int dx[4] = {-1 , 0 , 1 , 0} ;
+    int dy[4] = {0 , -1 , 0 , 1} ;
+    bool isValid(int x , int y , vector<vector<char>> &board){
+        if(x < 0 || y < 0 || x >= board.size() || y >= board[0].size() || board[x][y] == '.')   return false ;
+        else    return true ; 
+    }
+    bool dfs(int x , int y , int index , vector<vector<char>> &board , string &word){
+        if(index == word.length()){
+            return true ; 
+        }
+        if(board[x][y] != word[index]){
+            return false ; 
+        }
+        char temp = board[x][y] ; 
+        board[x][y] = '.' ;
+        bool possible = 0 ;
+        for(int i = 0 ; i < 4 ; i++){
+            if(isValid(x + dx[i] , y + dy[i] , board)){
+                possible = possible || dfs(x + dx[i] , y + dy[i]  , index + 1 , board , word) ;
+            }
+        }
+        possible = possible || (index == word.length() - 1) ;
+        board[x][y] = temp ; 
+        return possible ; 
+    }
+};
