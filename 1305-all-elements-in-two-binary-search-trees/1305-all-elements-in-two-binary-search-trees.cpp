087dc111d6967fd3a87d8af074e8cdcf7194722a
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-27 00:52:34 +0530

    Time: 194 ms (40.80%), Space: 84.3 MB (97.28%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,23 +12,43 @@
 class Solution {
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> list ; 
-        traverse(root1 , list) ; 
-        traverse(root2 , list) ;
-        sort(list.begin() , list.end()) ;
-        return list ;
+        vector<int> ans ; 
+        stack<TreeNode*> s1 , s2 ; 
+        pushLeft(root1 , s1) ; 
+        pushLeft(root2 , s2) ; 
+        while(!s1.empty() || !s2.empty()){
+            TreeNode* node = nullptr ; 
+            if(s1.empty()){
+                node = s2.top() ; 
+                s2.pop() ; 
+                ans.push_back(node->val) ; 
+                pushLeft(node->right , s2) ; 
+            } else if (s2.empty()) {
+                node = s1.top() ; 
+                s1.pop() ; 
+                ans.push_back(node->val) ; 
+                pushLeft(node->right , s1) ; 
+            } else {
+                if(s1.top()->val <= s2.top()->val){
+                    node = s1.top() ; 
+                    s1.pop() ;
+                    ans.push_back(node->val) ; 
+                    pushLeft(node->right , s1) ; 
+                } else {
+                    node = s2.top() ; 
+                    s2.pop() ; 
+                    ans.push_back(node->val) ; 
+                    pushLeft(node->right , s2) ; 
+                }
+            }
+        }
+        return ans ; 
     }
 private: 
-    void traverse(TreeNode* root , vector<int> &list){
-        if(!root)   return ; 
-        queue<TreeNode*> s ; 
-        s.push(root) ;
-        while(!s.empty()){
-            auto node = s.front() ; 
-            s.pop() ; 
-            list.push_back(node->val) ; 
-            if(node->left)  s.push(node->left) ; 
-            if(node->right) s.push(node->right) ;
+    void pushLeft(TreeNode* node , stack<TreeNode*> &s) {
+        while(node){
+            s.push(node) ; 
+            node = node->left ; 
         }
     }
 };
