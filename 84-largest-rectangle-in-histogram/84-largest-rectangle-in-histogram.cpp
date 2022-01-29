9685a4f6c6a5f76ff0be920ffdc8ffd2f5265773
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-29 13:32:51 +0530

    Time: 308 ms (9.29%), Space: 66.4 MB (36.96%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,57 +1,26 @@
-class SegmentTree {
-public: 
-    int start , end , min ; 
-    SegmentTree *left ;
-    SegmentTree *right ;
-    
-    SegmentTree (int start, int end) {
-        this->start = start ;
-        this->end = end ;
-        left = right = NULL ;
-    }  
-};
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        SegmentTree* root = build(heights , 0 , heights.size() - 1) ;
-        return maxAreaInThisSegment(heights , 0 , heights.size() - 1 , root) ;
-    }
-    
-    SegmentTree* build(vector<int>& heights, int start, int end) {
-        if (start > end) return NULL ;
-        SegmentTree *root = new SegmentTree(start, end) ;
-        if (start == end) {
-            root->min = start ;
-        return root ;
-        } else {
-          int middle = (start + end) / 2 ;
-          root->left = build(heights, start, middle) ;
-          root->right = build(heights, middle + 1, end) ;
-          root->min = heights[root->left->min] < heights[root->right->min] ? root->left->min : root->right->min ;
-          return root ;
+        int n = heights.size() ; 
+        vector<int> left(n , 0) , right(n , n) ; 
+        stack<int> s ; 
+        for(int i = 0 ; i < n ; i++) {
+            while(!s.empty() && heights[i] <= heights[s.top()]) s.pop() ;
+            if(s.empty())   left[i] = 0 ; //minimum element
+            else    left[i] = s.top() + 1 ;
+            s.push(i) ; 
         }
-    }
-    
-    int query(vector<int> &heights , int start , int end , SegmentTree* root) {
-        if(!root || end < root->start || start > root->end) return -1 ;
-        if (start <= root->start && end >= root->end) {
-            return root->min ;
+        while(!s.empty())   s.pop() ; 
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(!s.empty() && heights[i] <= heights[s.top()]) s.pop() ;
+            if(s.empty())   right[i] = n - 1 ; //minimum element
+            else    right[i] = s.top() - 1 ;
+            s.push(i) ; 
         }
-        int leftMin = query(heights , start , end , root->left) ;
-        int rightMin = query(heights , start , end , root->right) ; 
-        if(leftMin == -1)   return rightMin ;
-        if(rightMin == -1)  return leftMin ; 
-        return heights[leftMin] < heights[rightMin] ? leftMin : rightMin ; 
-    }
-    //calculates the maximum possible area in the histogram between indexes start and end
-    int maxAreaInThisSegment(vector<int> &heights , int start , int end , SegmentTree *root) {
-        if(start > end) {
-            return -1 ;
-        } 
-        if(start == end) {
-            return heights[start] ;
+        int max_area = 0 ; 
+        for(int i = 0 ; i < n ; i++) {
+            max_area = max(max_area , heights[i] * (right[i] - left[i] + 1)) ;
         }
-        int wholeMin = query(heights , start , end , root) ; 
-        return max({heights[wholeMin] * (end - start + 1) , maxAreaInThisSegment(heights , start , wholeMin - 1 , root) , maxAreaInThisSegment(heights , wholeMin + 1 , end , root)}) ;
+        return max_area ;
     }
 };
