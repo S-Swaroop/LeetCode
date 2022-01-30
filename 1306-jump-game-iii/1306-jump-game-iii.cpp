ee0ea9ef3f76766b068a5dc11cee53452dffc403
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-01-30 20:57:25 +0530

    Time: 36 ms (90.95%), Space: 31.2 MB (77.92%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size() ; 
-        vector<bool> vis(n , 0) ;
-        return dfs(arr , start , vis) ;
+        return bfs(arr , start) ;
     }
 private: 
     bool isValid(int index , vector<int> &arr , vector<bool> &vis) {
         if(index < 0 || index >= arr.size() || vis[index])    return false ; 
         else    return  true ; 
     }
-    bool dfs(vector<int> &arr , int start , vector<bool> &vis) {
+    bool bfs(vector<int> &arr , int start) {
         if (arr[start] == 0)    return true ; 
+        int n = arr.size() ;
+        vector<bool> vis(n , 0) ;
         vis[start] = 1 ;
-        if (isValid(start + arr[start] , arr , vis)) {
-            if (dfs(arr , start + arr[start] , vis)) {
-                return true ; 
-            }
-        } 
-        if (isValid(start - arr[start] , arr , vis)) {
-            if (dfs(arr , start - arr[start] , vis)) {
-                return true ; 
+        queue<int> q ; 
+        q.push(start) ;
+        while (!q.empty()) {
+            int index = q.front() ; 
+            q.pop() ; 
+            if (arr[index] == 0)    return true ; 
+            if (isValid(index + arr[index] , arr , vis)) {
+                q.push(index + arr[index]) ;
+                vis[index + arr[index]] = 1 ;
+            } 
+            if (isValid(index - arr[index] , arr , vis)) {
+                q.push(index - arr[index]) ;
+                vis[index - arr[index]] = 1 ;
             }
         }
         return false ; 
