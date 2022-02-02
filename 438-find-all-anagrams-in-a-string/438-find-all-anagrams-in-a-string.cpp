612d99b7bd0e98fb00235024f51ac7e7a4b7e271
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-02-02 12:08:30 +0530

    Time: 23 ms (49.84%), Space: 8.6 MB (76.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> findAnagrams(string s, string p) {
         int n = s.length() ; 
         int m = p.length() ; 
-        if(m > n){
+        if (m > n){
             return {} ; 
         }
         vector<int> fs(26,0) , fp(26,0) , ans ; 
-        for(int i = 0 ; i < m ; i++){
-            fs[s[i]-'a']++ ; 
-            fp[p[i]-'a']++ ; 
+        for (int i = 0 ; i < m ; i++){
+            fs[s[i] - 'a']++ ; 
+            fp[p[i] - 'a']++ ; 
         }
-        if(fs==fp)  ans.push_back(0) ; 
-        for(int i = m ; i < n ; i++){
-            fs[s[i]-'a']++ ; 
-            fs[s[i-m]-'a']-- ;
-            if(fs==fp){
+        if (fs == fp)  ans.push_back(0) ; 
+        for (int i = m ; i < n ; i++){
+            fs[s[i] - 'a']++ ; 
+            fs[s[i - m] - 'a']-- ;
+            if (fs == fp){
                 ans.push_back(i-m+1) ; 
             }
         }
