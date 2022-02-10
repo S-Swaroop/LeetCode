e19607f5ad3c222ac30c7cbc92a02a5c072b43f0
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-02-10 18:36:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (66.52%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char , int> f ; 
-        for (char &i : s)   f[i]++ ; 
-        for (char &i : t) {
-            if (f.find(i) == f.end() || f[i] == 0) {
-                return i ; 
-            } else {
-                f[i]-- ;
-                if (f[i] == 0)  f.erase(i) ; 
-            }
-        }
-        return f.begin()->first ;
+        int hash = 0 ;
+        for (char &c : t)   hash += (c - 'a') ;
+        for (char &c : s)   hash -= (c - 'a') ;
+        return (char)('a' + hash) ;
     }
 };
