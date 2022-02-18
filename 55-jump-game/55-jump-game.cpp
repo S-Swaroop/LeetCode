857528b465010dcc2624b09b2c34600b6a29652f
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-02-18 12:05:14 +0530

    Time: 79 ms (67.38%), Space: 48.2 MB (96.40%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& a) {
+        int n = a.size() ; 
+        if(n==1){//only one element so we are already at the last index
+            return true ; 
+        }
+        if(a[0]==0){ //base case if no jumps possible
+            return false ; 
+        }
+        for(int i = 1 ; i < n-1 ; i++){
+            a[i] = max(a[i] , a[i-1] - 1) ;//comparing if its better to take the jump now or continue the last jump
+            if(a[i]==0) return false ; 
+        }
+        return true ; 
+    }
+};
