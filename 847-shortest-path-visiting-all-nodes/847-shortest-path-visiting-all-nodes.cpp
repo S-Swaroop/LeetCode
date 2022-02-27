89795fb123589c2f141c1fc84ac6e514c40db3c5
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-02-27 11:39:45 +0530

    Time: 234 ms (23.41%), Space: 18.3 MB (41.81%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -1,81 +1,39 @@
 class Solution {
 public:
-    // Class to carry three values at a time
-    class tuple
-    {
-        public:
-        int node; // on which current node we are standing
-        int mask; // mask of that node
-        int cost; // cost of path explore by this node
-        tuple(int node, int mask, int cost)
-        {
-            this -> node = node;
-            this -> mask = mask;
-            this -> cost = cost;
-        }
+    class triplet {
+    public :
+        int node , mask , cost ; 
+        triplet(int _node , int _mask , int _cost) {
+            this -> node = _node ; 
+            this -> mask = _mask ; 
+            this -> cost = _cost ; 
+        };
     };
-    
-    
     int shortestPathLength(vector<vector<int>>& graph) {
-        // total number of nodes present
-        int n = graph.size(); // extracting size of graph
-        
-        
-        queue<tuple> q; // queue of class tuple type
-        
-         // set to take care which path we have already visited
-        set<pair<int, int>> vis;
-        
-        int all = (1 << n) - 1; // if all nodes visited then
-            
-        // we don't know which node will give us shortest path so intially for all nodes we will store in our queue
-        for(int i = 0; i < n; i++)
-        {
-            int maskValue = (1 << i); // 2 ^ i
-            
-            tuple thisNode(i, maskValue, 1); // make a tuple for every nod
-            
-            q.push(thisNode); // push tuple into our queue
-            
-            vis.insert({i, maskValue}); // also update into our set
+        int n = graph.size() ; 
+        queue<triplet> q ; 
+        set<pair<int , int>> vis ; 
+        for (int i = 0 ; i < n ; i++) {
+            triplet curr_node (i , (1 << i) , 1) ;
+            vis.insert({i , (1 << i)}) ;
+            q.push(curr_node) ;
         }
-        
-        // Implementing BFS
-        while(q.empty() == false) // until queue is not empty
-        {
-            tuple curr = q.front(); // extracting front tuple
-            q.pop(); // pop from queuu
-            
-            // if mask value becomes all, that means we have visited all of our nodes, so from here return cost - 1
-            if(curr.mask == all) 
-            {
-                return curr.cost - 1;
+        while (!q.empty()) {
+            triplet curr = q.front() ; 
+            q.pop() ; 
+            if (curr.mask == ((1 << n) - 1)) {
+                return curr.cost - 1 ;
             }
-            
-            // if not, start exploring in its adjcant
-            for(auto &adj: graph[curr.node])
-            {
-                int bothVisitedMask = curr.mask; // current mask
-                
-                // we are moving from one node o anthor node
-                bothVisitedMask = bothVisitedMask | (1 << adj); 
-                
-                // make tuple of this path
-                tuple ThisNode(adj, bothVisitedMask, curr.cost + 1);
-                
-                // if this path is not explored i.e
-                // if it is not present in our set then,
-                if(vis.find({adj, bothVisitedMask}) == vis.end())
-                {
-                    vis.insert({adj, bothVisitedMask}); // insert into set
-                    
-                    q.push(ThisNode); // also insert into queue
+            for (int &i : graph[curr.node]) {
+                int both_vis_mask = curr.mask ; 
+                both_vis_mask = both_vis_mask | (1 << i) ;
+                triplet curr_node(i , both_vis_mask , curr.cost + 1) ;
+                if (vis.find({i , both_vis_mask}) == vis.end()) {
+                    vis.insert({i , both_vis_mask}) ;
+                    q.push(curr_node) ;
                 }
             }
-            
         }
-        
-        // return -1, but this condition will never come
-        return -1;
+        return -1 ;
     }
 };
