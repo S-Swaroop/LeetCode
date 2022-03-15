f24724b5c0b89f9374cee96a31e94cdf011d1d85
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-03-15 23:40:52 +0530

    Time: 24 ms (84.66%), Space: 8.3 MB (97.32%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int numSplits(string s) {
-        map<char , int> left , right ; 
+        vector<int> left(26 , 0) , right(26 , 0) ;
+        int l = 0 , r = 0 , ans = 0 ;
         for (char &i : s) {
-            right[i]++ ;
+            r += (++right[i - 'a'] == 1) ;
         }
-        int ans = 0 ; 
         for (char &i : s) {
-            if (right.find(i) != right.end()) {
-                right[i]-- ;
-                if (right[i] == 0) {
-                    right.erase(i) ;
-                }
-            }
-            left[i]++ ;
-            if (left.size() == right.size()) {
-                ans++ ;
-            }
+            l += (++left[i - 'a'] == 1) ;
+            r -= (--right[i - 'a'] == 0) ;
+            ans += (r == l) ;
         }
         return ans ; 
     }
