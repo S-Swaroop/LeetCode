b43f311a009dab6df95595d5312fca94a66e85cc
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-03-15 23:36:56 +0530

    Time: 49 ms (45.88%), Space: 8.5 MB (65.14%) - LeetHub

---
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        unordered_map<char , int> left , right ; 
+        for (char &i : s) {
+            right[i]++ ;
+        }
+        int ans = 0 ; 
+        for (char &i : s) {
+            if (right.find(i) != right.end()) {
+                right[i]-- ;
+                if (right[i] == 0) {
+                    right.erase(i) ;
+                }
+            }
+            left[i]++ ;
+            if (left.size() == right.size()) {
+                ans++ ;
+            }
+        }
+        return ans ; 
+    }
+};
