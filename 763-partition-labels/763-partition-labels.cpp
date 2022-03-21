5850d1c21a4d00faa65e992b1c6d3670f454dcfd
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-03-22 00:16:44 +0530

    Time: 3 ms (85.70%), Space: 6.5 MB (87.54%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> lastOcc(26 , 0) ;
-        int n = s.length() ;
-        for(int i = 0 ; i < n ; i++){
-            lastOcc[s[i] - 'a'] = i ; 
+        vector<int> last(26 , -1) ; 
+        for (int i = 0 , n = s.length() ; i < n ; i++) {
+            last[s[i] - 'a'] = max(last[s[i] - 'a'] , i) ; 
         }
-        int f = 0 ; 
-        vector<int> ans ;  
-        int lst = 0 ; 
-        for(int i = 0 ; i < n ; i++){
-            f = max(f , lastOcc[s[i] - 'a']) ;
-            if(i == f){
-                ans.push_back(i - lst + 1) ; 
-                lst = i + 1 ; 
+        vector<int> ans ; 
+        for (int i = 0 , n = s.length() ; i < n ; ) {
+            int j , end = last[s[i] - 'a'] ;
+            for (j = i ; j <= end ; j++) {
+                end = max(end , last[s[j] - 'a']) ;
             }
+            ans.push_back(j - i) ; 
+            i = j ; 
         }
         return ans ; 
     }
