50884913a1b4536723bc390203e43d70e3cd51a7
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-03-28 02:43:46 +0530

    Time: 12 ms (83.38%), Space: 10.6 MB (19.44%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        auto count = [&](int row) {
+            if (mat[row][0] == 0)  return 0 ;
+            int l = -1 , r = mat[row].size() ;
+            while (r - l > 1) {
+                int m = l + (r - l) / 2 ;
+                if (mat[row][m]) {
+                    l = m ; 
+                } else {
+                    r = m ;
+                }
+            }
+            return r + 1 ;
+        }; 
         set<pair<int , int>> s ; 
         for (int i = 0 , n = mat.size() ; i < n ; i++) {
-            s.insert({count(mat[i]) , i}) ;
+            s.insert({count(i) , i}) ;
             if (s.size() > k) {
                 s.erase(--s.end()) ;
             }
@@ -13,19 +26,5 @@ public:
             ans.push_back(index) ;
         }
         return ans ;
-    }
-private: 
-    int count (vector<int> &a) {
-        if (a[0] == 0)  return 0 ;
-        int l = -1 , r = a.size() ;
-        while (r - l > 1) {
-            int m = l + (r - l) / 2 ;
-            if (a[m]) {
-                l = m ; 
-            } else {
-                r = m ;
-            }
-        }
-        return r + 1 ;
     }
 };
