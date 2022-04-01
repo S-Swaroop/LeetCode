96d7edbd5a9ac262c7ce20c10265f67bf6274208
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-04-01 18:40:50 +0530

    Time: 7 ms (35.49%), Space: 7 MB (95.75%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
-        memset(memo , -1 , sizeof(memo)) ;
         int n = nums.size() ; 
-        vector<int> pref(n + 1 , 0) ;
-        for (int i = 0 ; i < n ; i++) {
-            pref[i + 1] += pref[i] + nums[i] ;
-        }
-        return dp(0 , m , pref) ;
-    }
-private: 
-    int memo[1001][51] ;
-    int dp (int i , int m , vector<int> &pref) { // best answer till ith index , having made m sub arrays
-        int n = pref.size() - 1 ;
-        if (memo[i][m] != -1) {
-            return memo[i][m] ;
-        }
-        if (m == 1) {
-            return memo[i][m] = pref[n] - pref[i] ;
-        }
-        int &res = memo[i][m] ;
-        res = INT_MAX ; 
-        for (int j = i ; j <= n - m ; j++) {
-            int curr_sub_sum = pref[j + 1] - pref[i] ;
-            int largest_sub_sum = max(curr_sub_sum , dp (j + 1 , m - 1 , pref)) ;
-            res = min(res , largest_sub_sum) ;
-            if (curr_sub_sum >= largest_sub_sum) { // won't find any larger sum than this so break
-                break ;
+        int low = *max_element(nums.begin() , nums.end()) , high = accumulate(nums.begin() , nums.end() , 0) ; 
+        int ans ;
+        auto possible = [&] (int sum) {
+            int curr_sum = 0 , splits = 0 ;
+            for (int i = 0 ; i < n ; i++) {
+                if (curr_sum + nums[i] > sum) {
+                    splits++ ;
+                    curr_sum = nums[i] ;
+                } else {
+                    curr_sum += nums[i] ;
+                }
+            }
+            return splits + 1 <= m ;
+        } ;
+        while (high >= low) {
+            int mid = low + ((high - low) / 2) ;
+            if (possible (mid)) {
+                ans = mid ; 
+                high = mid - 1 ; 
+            } else {
+                low = mid + 1 ;
             }
         }
-        return res ;
+        return ans ; 
     }
 };
