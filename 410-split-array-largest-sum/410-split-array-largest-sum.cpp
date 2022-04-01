7df8cdfd1c4ca00789fba620a219e2276676fdd4
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-04-01 17:17:08 +0530

    Time: 72 ms (25.26%), Space: 7.3 MB (31.69%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -11,7 +11,7 @@ public:
     }
 private: 
     int memo[1001][51] ;
-    int dp (int i , int m , vector<int> &pref) {
+    int dp (int i , int m , vector<int> &pref) { // best answer till ith index , having made m sub arrays
         int n = pref.size() - 1 ;
         if (memo[i][m] != -1) {
             return memo[i][m] ;
@@ -19,15 +19,16 @@ private:
         if (m == 1) {
             return memo[i][m] = pref[n] - pref[i] ;
         }
-        int res = INT_MAX ; 
+        int &res = memo[i][m] ;
+        res = INT_MAX ; 
         for (int j = i ; j <= n - m ; j++) {
-            int sum = pref[j + 1] - pref[i] ;
-            int best = max(sum , dp (j + 1 , m - 1 , pref)) ;
-            res = min(res , best) ;
-            if (sum >= best) {
+            int curr_sub_sum = pref[j + 1] - pref[i] ;
+            int largest_sub_sum = max(curr_sub_sum , dp (j + 1 , m - 1 , pref)) ;
+            res = min(res , largest_sub_sum) ;
+            if (curr_sub_sum >= largest_sub_sum) { // won't find any larger sum than this so break
                 break ;
             }
         }
-        return memo[i][m] = res ;
+        return res ;
     }
 };
