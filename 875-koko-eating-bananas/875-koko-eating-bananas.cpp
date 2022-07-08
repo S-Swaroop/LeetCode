66aff9e32ba842515c1bacd9968d8a3fa265c2c5
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-07-08 14:48:44 +0530

    Time: 50 ms (86.86%), Space: 19 MB (27.69%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        //utility function to check if the we can eat all the bananas at rate k in h hours
-        auto possible = [&](int k)->bool{ 
-            int time_taken = 0 ; 
-            for(int &i : piles){
-                time_taken += (i + k - 1) / k ; 
-            }
-            return (time_taken <= h) ; 
-        };
-        long long low = 0 , high = 1e10 ;
-        int ans = 0 ; 
-        while(high - low > 1){
+        int low = 0 , high = 1e9 + 1 ;
+        while (high - low > 1) {
             int mid = low + ((high - low) >> 1) ; 
-            if(possible(mid)){
-                high = mid ;
-                ans = mid ;
-            }else{
-                low = mid ;
+            if (possible(piles , mid , h)) {
+                high = mid ; 
+            } else {
+                low = mid ; 
             }
         }
-        return ans ; 
+        return high ;
+    }
+private: 
+    bool possible (vector<int> &piles , int k , int h) {
+        for (int &i : piles) {
+            h -= (i + k - 1) / k ; 
+        }
+        return h >= 0 ;
     }
 };
