7747910775d2a3d157d18263f05439146896a483
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-07-13 21:19:34 +0530

    Time: 5 ms (27.13%), Space: 6.5 MB (88.94%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length() , len = 0 ; 
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            if (isalpha(s[i])) {
+                len++ ; 
+            } else if (len > 0) {
+                break ;
+            } else {
+                continue ;
+            }
+        }
+        return len ;
+    }
+};
