2fe4fbed57b63d3c9aa20b8a42e7daeceddd3032
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-08-29 12:49:35 +0530

    Time: 603 ms (92.62%), Space: 139.1 MB (95.54%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -3,22 +3,23 @@ public:
     long long countPairs(int n, vector<vector<int>>& edges) {
         long long ans = 0 ;
         parent.resize(n , 0) ;
+        _size.resize(n , 1) ;
         _rank.resize(n , 0) ;
         iota(parent.begin() , parent.end() , 0) ;
         vector<int> comp(n , 0) ;
         for (auto &i : edges) {
             merge(i[0] , i[1]) ;
         }
+        // for (int i = 0 ; i < n ; i++) {
+        //     comp[findParent(i)]++ ;
+        // }
         for (int i = 0 ; i < n ; i++) {
-            comp[findParent(i)]++ ;
+            ans += (long long)(n - _size[i]) * (long long)_size[i] ;
         }
-        for (int i = 0 ; i < n ; i++) {
-            ans += (long long)(n - comp[i]) * (long long)comp[i] ;
-        }
-        return ans / 2 ;
+        return (ans >> 1) ;
     }
 private: 
-    vector<int> parent , _rank ;
+    vector<int> parent , _rank , _size ;
     int findParent (int u) {
         if (parent[u] == u) {
             return u ;
@@ -32,11 +33,17 @@ private:
         if (u != v) {
             if (_rank[u] > _rank[v]) {
                 parent[u] = v ;
+                _size[v] += _size[u] ;
+                _size[u] = 0 ;
             } else if (_rank[v] > _rank[u]) {
                 parent[v] = u ;
+                _size[u] += _size[v] ;
+                _size[v] = 0 ;
             } else {
                 parent[u] = v ;
                 _rank[u]++ ;
+                _size[v] += _size[u] ;
+                _size[u] = 0 ;
             }
         }
     }
