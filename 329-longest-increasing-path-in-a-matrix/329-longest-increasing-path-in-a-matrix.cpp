cd5618a45680910e2337490947763b9591b4b780
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-08-30 17:25:16 +0530

    Time: 66 ms (65.77%), Space: 16.1 MB (49.37%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int n = matrix.size() ; 
-        int m = matrix[0].size() ; 
-        dp.resize(n , vector<int>(m , 0)) ;
-        int ans = 0 ; 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                ans = max(ans , dfs(i , j , matrix)) ;
+        int n = matrix.size() , m = matrix[0].size() ;
+        memo.resize(n + 1 , vector<int> (m + 1 , 0)) ;
+        int ans = 0 ;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                ans = max(ans , dp(i , j , matrix)) ;
             }
         }
         return ans ;
     }
 private: 
-    vector<vector<int>> dp ;
+    vector<vector<int>> memo ;
     int dx[4] = {-1 , 0 , 1 , 0} ;
     int dy[4] = {0 , -1 , 0 , 1} ;
     
-    bool isValid(int x , int y){
-        if(x < 0 || y < 0 || x >= dp.size() || y >= dp[0].size()) return false ; 
-        else    return true ; 
+    bool isValid (int x , int y , vector<vector<int>> &matrix) {
+        return (x >= 0 && x < (int)matrix.size() && y >= 0 && y < (int)matrix[0].size()) ;
     }
     
-    int dfs(int x , int y , vector<vector<int>> &matrix){
-        if(dp[x][y])    return dp[x][y] ;
-        for(int i = 0 ; i < 4 ; i++){
-            if(isValid(x + dx[i] , y + dy[i]) && matrix[x + dx[i]][y + dy[i]] > matrix[x][y]){
-                dp[x][y] = max(dp[x][y] , dfs(x + dx[i] , y + dy[i] , matrix)) ;
-            }   
+    int dp (int x , int y , vector<vector<int>> &matrix) {
+        if (memo[x][y]) {
+            return memo[x][y] ;
+        } else {
+            int res = 1 ;
+            for (int i = 0 ; i < 4 ; i++) {
+                if (isValid(x + dx[i] , y + dy[i] , matrix) && matrix[x + dx[i]][y + dy[i]] > matrix[x][y]) {
+                    res = max(res , dp(x + dx[i] , y + dy[i] , matrix) + 1) ;
+                }
+            }
+            return memo[x][y] = res ;
         }
-        return ++dp[x][y] ; 
     }
 };
