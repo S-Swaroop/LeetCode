9b7d8c9e24e3c5fbad092c6375ef9d8cafaf2999
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-10 11:47:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        int n = palindrome.length() ;
-        if (n == 1) {
+        if (palindrome.length() == 1) {
             return "" ;
-        }
-        for (int i = 0 ; i < n / 2 ; i++) {
-            if (palindrome[i] != 'a') {
-                palindrome[i] = 'a' ; 
-                return palindrome ;
+        } else {
+            for (int i = 0 ; i < palindrome.length() / 2 ; i++) {
+                if (palindrome[i] != 'a') {
+                    palindrome[i] = 'a' ; 
+                    return palindrome ;
+                }
             }
+            palindrome.back() = 'b' ; 
+            return palindrome ;
         }
-        palindrome[n - 1] = 'b' ; 
-        return palindrome ;
     }
 };
