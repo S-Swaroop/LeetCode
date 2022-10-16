95e3a82a354e9cd488e1ad96eb14ea9bbd403261
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-16 17:20:30 +0530

    Time: 47 ms (81.63%), Space: 7.2 MB (93.10%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
-        if(d > jobDifficulty.size())    return -1 ; 
         memset(memo , -1 , sizeof(memo)) ;
-        return dp(0 , d , jobDifficulty , jobDifficulty.size()) ;
+        return dp(0 , d , jobDifficulty) ;
     }
-private : 
-    int memo[301][11] ; 
-    int dp(int i , int k , vector<int>& jobDifficulty , int n){
-        if(k == 1){
-            return memo[i][k] = *max_element(jobDifficulty.begin() + i , jobDifficulty.end()) ; 
+private: 
+    int memo[301][11] ;
+    int dp (int index , int daysLeft , vector<int> &jobDifficulty) {
+        if (daysLeft > (int)jobDifficulty.size()) {
+            return -1 ;
+        } else if (index >= (int)jobDifficulty.size()) {
+            return 0 ;
+        } else if (memo[index][daysLeft] != -1) {
+            return memo[index][daysLeft] ;  
+        } else if (daysLeft == 1) {
+            return memo[index][daysLeft] = *max_element(jobDifficulty.begin() + index , jobDifficulty.end()) ;
+        } else {
+            int &res = memo[index][daysLeft] ;
+            res = INT_MAX ;
+            int n = jobDifficulty.size() ;
+            int _max = 0 ;
+            for (int i = index ; i < n - daysLeft + 1 ; i++) {
+                _max = max(_max , jobDifficulty[i]) ;
+                res = min(res , dp(i + 1 , daysLeft - 1 , jobDifficulty) + _max) ;
+            }
+            return res ;
         }
-        if(memo[i][k] != -1)    return memo[i][k] ; 
-        int &res = memo[i][k] ; 
-        res = INT_MAX ; 
-        int _max = 0 ; 
-        for(int j = i ; j <= n - k ; j++){
-            _max = max(_max , jobDifficulty[j]) ;
-            res = min(res , _max + dp(j + 1 , k - 1 , jobDifficulty , n)) ;
-        }
-        return res ; 
     }
 };
