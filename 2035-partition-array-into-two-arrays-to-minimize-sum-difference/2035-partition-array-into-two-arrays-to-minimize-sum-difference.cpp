5e35bfa89feb2c28d5d80bd07b4df886337f57ce
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-16 18:09:13 +0530

    Time: 1943 ms (7.66%), Space: 70.3 MB (69.66%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -6,8 +6,8 @@ public:
         int totalSum = accumulate(nums.begin() , nums.end() , 0) ;
         int target = totalSum / 2 ;
         vector<vector<int>> left(n / 2 + 1) , right(n / 2 + 1) ;
-        generate(0 , n / 2 - 1 , nums , left , right) ; 
-        // generate(n / 2 , n - 1 , nums , right) ;
+        generate(0 , n / 2 - 1 , nums , left) ; 
+        generate(n / 2 , n - 1 , nums , right) ;
         for (int i = 0  ; i < n / 2 + 1 ; i++) {
             for (int j = 0 ; j < (int)left[i].size() ; j++) {
                 auto it = lower_bound(right[n / 2 - i].begin() , right[n / 2 - i].end() , target - left[i][j]) ;
@@ -29,24 +29,21 @@ public:
         return ans ;
     }
 private: 
-    void generate (int start , int end , vector<int> &nums , vector<vector<int>> &a , vector<vector<int>> &b) {
+    void generate (int start , int end , vector<int> &nums , vector<vector<int>> &a) {
         int n = end - start + 1 ;
         int lim = 1 << n ;
         for (int i = 0 ; i < lim ; i++) {
             int chosen = __builtin_popcount(i) ;
-            int sum1 = 0 , sum2 = 0 ;
+            int sum = 0 ;
             for (int j = start ; j <= end ; j++) {
                 if ((1 << (j - start)) & i) {
-                    sum1 += nums[j] ;
-                    sum2 += nums[j + end + 1] ;
+                    sum += nums[j] ;
                 }
             }
-            b[chosen].push_back(sum2) ;
-            a[chosen].push_back(sum1) ;
+            a[chosen].push_back(sum) ;
         }
         for (int i = 0 ; i < (int)a.size() ; i++) {
             sort(a[i].begin() , a[i].end()) ;
-            sort(b[i].begin() , b[i].end()) ;
         }
     }
     
