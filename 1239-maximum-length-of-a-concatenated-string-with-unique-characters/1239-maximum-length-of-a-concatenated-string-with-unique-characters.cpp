92dd3d88b0836c4546d7919a2ef81229c7cd717f
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-24 17:38:17 +0530

    Time: 4 ms (95.93%), Space: 8.1 MB (98.50%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        int n = arr.size() ;
-        int lim = 1 << n ; 
-        int max_length = 0 ; 
-        for (int mask = 1 ; mask < lim ; mask++) {
-            int length = 0 ;
-            vector<int> f(26 , 0) ; 
-            for (int i = 0 ; i < n ; i++) {
-                if (mask & (1 << i)) {
-                    for (char &j : arr[i]) {
-                        f[j - 'a']++ ;
-                        length++ ;
-                    }
+        return helper(0 , 0 , arr) ;
+    }
+private: 
+    int helper (int index , int mask , vector<string> &arr) {
+        if (index >= (int)arr.size()) {
+            return 0 ;
+        } else {
+            int t = mask ;
+            bool possible = true ; 
+            for (char &i : arr[index]) {
+                if (t & (1 << (i - 'a'))) { // if true then this character is already present in the mask
+                    possible = false ;
+                    break ;
                 }
+                t |= (1 << (i - 'a')) ;
             }
-            bool unique = true ;
-            for (int i = 0 ; i < 26 ; i++) {
-                unique &= (f[i] < 2) ;
-            }
-            if (unique) {
-                max_length = max(max_length , length) ;
+            if (!possible) {
+                return helper (index + 1 , mask , arr) ;
+            } else {
+                return max(helper(index + 1 , mask , arr) , (int)arr[index].length() + helper(index + 1 , t , arr)) ;
             }
         }
-        return max_length ;
     }
 };
