226ee8ff7f88d4c82828916b296b2021fa08b62a
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-24 18:55:18 +0530

    Time: 5 ms (93.90%), Space: 8 MB (99.91%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,60 +1,70 @@
-// class Solution {
-// public:
-//     int maxLength(vector<string>& arr) {
-//         return helper(0 , 0 , arr) ;
-//     }
-// private: 
-//     int helper (int index , int mask , vector<string> &arr) {
-//         if (index >= (int)arr.size()) {
-//             return 0 ;
-//         } else {
-//             int t = mask ;
-//             bool possible = true ; 
-//             for (char &i : arr[index]) {
-//                 if (t & (1 << (i - 'a'))) { // if true then this character is already present in the mask
-//                     possible = false ;
-//                     break ;
-//                 }
-//                 t |= (1 << (i - 'a')) ;
-//             }
-//             if (!possible) {
-//                 return helper (index + 1 , mask , arr) ;
-//             } else {
-//                 return max(helper(index + 1 , mask , arr) , (int)arr[index].length() + helper(index + 1 , t , arr)) ;
-//             }
-//         }
-//     }
-// };
+/*
+ * Smart Recursion
+*/
+
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        int n = arr.size() ;
-        int lim = 1 << n ; // this is our limit 
-        int max_length = 0 ; 
-        // running through all masks from 1 to 2^n - 1
-        for (int mask = 1 ; mask < lim ; mask++) { 
-            int length = 0 ;
-            bool unique = true ;
-            vector<int> f(26 , 0) ; 
-            for (int i = 0 ; i < n && unique ; i++) {
-                if (mask & (1 << i)) { // checking whether this element is present in mask
-                    for (char &j : arr[i]) {
-                        f[j - 'a']++ ;
-                        if (f[j - 'a'] > 1) {
-                            unique = false ; 
-                            break ;
-                        }
-                        length++ ;
-                    }
-                }
-                if (!unique) {
+        return helper(0 , 0 , arr) ;
+    }
+private: 
+    int helper (int index , int mask , vector<string> &arr) {
+        if (index >= (int)arr.size()) {
+            return 0 ;
+        } else {
+            int t = mask ;
+            bool possible = true ; 
+            for (char &i : arr[index]) {
+                if (t & (1 << (i - 'a'))) { // if true then this character is already present in the mask
+                    possible = false ;
                     break ;
                 }
+                t |= (1 << (i - 'a')) ;
             }
-            if (unique) {
-                max_length = max(max_length , length) ;
+            if (!possible) {
+                return helper (index + 1 , mask , arr) ;
+            } else {
+                return max(helper(index + 1 , mask , arr) , (int)arr[index].length() + helper(index + 1 , t , arr)) ;
             }
         }
-        return max_length ;
     }
 };
+
+
+/*
+ * Bitmasking/exhaustive search method
+*/
+
+// class Solution {
+// public:
+//     int maxLength(vector<string>& arr) {
+//         int n = arr.size() ;
+//         int lim = 1 << n ; // this is our limit 
+//         int max_length = 0 ; 
+//         // running through all masks from 1 to 2^n - 1
+//         for (int mask = 1 ; mask < lim ; mask++) { 
+//             int length = 0 ;
+//             bool unique = true ;
+//             vector<int> f(26 , 0) ; 
+//             for (int i = 0 ; i < n && unique ; i++) {
+//                 if (mask & (1 << i)) { // checking whether this element is present in mask
+//                     for (char &j : arr[i]) {
+//                         f[j - 'a']++ ;
+//                         if (f[j - 'a'] > 1) {
+//                             unique = false ; 
+//                             break ;
+//                         }
+//                         length++ ;
+//                     }
+//                 }
+//                 if (!unique) {
+//                     break ;
+//                 }
+//             }
+//             if (unique) {
+//                 max_length = max(max_length , length) ;
+//             }
+//         }
+//         return max_length ;
+//     }
+// };
