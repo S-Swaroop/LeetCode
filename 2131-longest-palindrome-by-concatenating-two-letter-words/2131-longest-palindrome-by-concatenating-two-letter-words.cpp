0995b409096fea9655e655e18fb36fc3d74ea3f8
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-25 12:44:00 +0530

    Time: 591 ms (72.12%), Space: 168.1 MB (26.75%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int ans = 0 ;
-        vector<int> f(852 , 0) ;
+        vector<int> f(781 , 0) ;
         for (string &s : words) {
-            if (f[(s[1] - 'a' + 1) + ((s[0] - 'a' + 1) * 31)]) {
+            if (f[(s[1] - 'a' + 1) + ((s[0] - 'a' + 1) * 29)]) {
                 ans += 4 ;
-                f[(s[1] - 'a' + 1) + ((s[0] - 'a' + 1) * 31)]-- ;
+                f[(s[1] - 'a' + 1) + ((s[0] - 'a' + 1) * 29)]-- ;
             } else {
-                f[(s[0] - 'a' + 1) + ((s[1] - 'a' + 1) * 31)]++ ;
+                f[(s[0] - 'a' + 1) + ((s[1] - 'a' + 1) * 29)]++ ;
             }
         }
         for (string &s : words) {
-            if (s[0] == s[1] && f[(s[0] - 'a' + 1) + ((s[1] - 'a' + 1) * 31)]) {
+            if (s[0] == s[1] && f[(s[0] - 'a' + 1) + ((s[1] - 'a' + 1) * 29)]) {
                 ans += 2 ; 
                 break ;
             }
