871bdd6cedc566036aa69d7f390a2179f75932ac
Sharan Swaroop <itsmesharan2000@gmail.com>
2022-10-27 11:14:44 +0530

    Time: 116 ms (69.45%), Space: 12.1 MB (38.19%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int n = img1.size() ; 
-        int best_overlap = 0 ; 
-        for (int dx = -1 * n ; dx <= n ; dx++) {
-            for (int dy = -1 * n ; dy <= n ; dy++) {
-                int overlap = 0 ;
-                for (int i = 0 ; i < n ; i++) {
-                    for (int j = 0 ; j < n ; j++) {
-                        if ((i + dx < n && i + dx >= 0) && (j + dy < n && j + dy >= 0)) {
-                            overlap += (img1[i + dx][j + dy] & img2[i][j]) ;
-                        }
-                    }
-                }
-                best_overlap = max(best_overlap , overlap) ;
+        int n = img1.size() ;
+        vector<int> a , b ; 
+        unordered_map<int , int> count ;
+        for (int i = 0 ; i < n * n ; i++) {
+            if (img1[i / n][i % n] == 1) {
+                a.push_back((i / n) * 100 + (i % n)) ;
+            }
+            if (img2[i / n][i % n] == 1) {
+                b.push_back((i / n) * 100 + (i % n)) ;
             }
         }
-        return best_overlap ;
+        
+        for (int i : a) {
+            for (int j : b) {
+                count[j - i]++ ;
+            }
+        }
+        
+        int ans = 0 ; 
+        for (auto &i : count) {
+            ans = max(ans , i.second) ;
+        }
+        
+        return ans ;
+        
+        // int n = img1.size() ; 
+        // int best_overlap = 0 ; 
+        // for (int dx = -1 * n ; dx <= n ; dx++) {
+        //     for (int dy = -1 * n ; dy <= n ; dy++) {
+        //         int overlap = 0 ;
+        //         for (int i = 0 ; i < n ; i++) {
+        //             for (int j = 0 ; j < n ; j++) {
+        //                 if ((i + dx < n && i + dx >= 0) && (j + dy < n && j + dy >= 0)) {
+        //                     overlap += (img1[i + dx][j + dy] & img2[i][j]) ;
+        //                 }
+        //             }
+        //         }
+        //         best_overlap = max(best_overlap , overlap) ;
+        //     }
+        // }
+        // return best_overlap ;
     }
 };
